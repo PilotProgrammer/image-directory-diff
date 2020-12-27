8cc29e01e1dc2c073fd8288970dd5693f068ac53
PilotProgrammer <17863859+PilotProgrammer@users.noreply.github.com>
2020-12-26 21:10:35 -0500

    WIP

--- src/Program.ts
+++ src/Program.ts
@@ -14,14 +14,24 @@ console.log(`haystackDir: ${haystackDir} needlesDir: ${needlesDir}`)
 // '/Library/haystack/1.JPG' is 1557620823e163c4c69bf7970df6e007fed8741613ded73009dd93eed04586ce
 // const fileName = '/Library/haystack/1.JPG'
 const fileName = '/Library/needles/1.JPG'
+const fileName2 = '/Library/needles/2.JPG'
+const fileName3 = '/Library/haystack/2.JPG'
 
 
 export class Program {
   public static async main() {
     const file = new File(fileName)
     const fileType = await file.getFileMimeType()
-    const fileHash = await file.checksum
-    console.log(`READ ${fileType} ${fileHash} ${fileName}`);
+    const fileHash = await file.getChecksum()
+    const fileStats = await file.getTotalFileBytes()
+    console.log(`READ ${fileType} ${fileHash} ${fileName} stats ${JSON.stringify(fileStats)}`);
+
+    const file2 = new File(fileName2)
+    const file3 = new File(fileName3)
+
+    console.log(`file1 == file2? ${await file.equals(file2)}`)
+    console.log(`file2 == file3? ${await file2.equals(file3)}`)
+
   }
 }
 Program.main()
--- src/lib/File.ts
+++ src/lib/File.ts
@@ -4,28 +4,69 @@ import { HashCreater } from "./HashCreater";
 const FileType = require('file-type');
 
 export class File {
-
   public constructor(private filePath: string) {
     // this._allFiles = this.walkFiles(this.directoryName, []);
   }
 
   private _hashCreater: HashCreater;
   private _fileMimeType: string;
+  private _fileSize: number
 
-  get checksum() {
+  public async equals(otherFile: File) {
+    // TODO
+
+    // check file name
+    console.log(`this.fileName != otherFile.fileName? ${this.fileName != otherFile.fileName}`)
+    if (this.fileName != otherFile.fileName)
+    return false
+    
+    console.log(`this.checksum != otherFile.checksum? ${await this.getChecksum() != await otherFile.getChecksum()}`)
+    // file hash
+    if (await this.getChecksum() != await otherFile.getChecksum())
+    return false
+    
+    // file size
+    console.log(`this.getTotalFileBytes() != otherFile.getTotalFileBytes()? ${this.getTotalFileBytes() != otherFile.getTotalFileBytes()}`)
+    if (this.getTotalFileBytes() != otherFile.getTotalFileBytes())
+    return false
+    
+    // file type
+    console.log(`this.getFileMimeType() != otherFile.getFileMimeType()? ${await this.getFileMimeType() != await otherFile.getFileMimeType()}`)
+    if (await this.getFileMimeType() != await otherFile.getFileMimeType())
+      return false
+
+    // if movie, duration, other movie or picture specs that can be verified?
+    return true
+  }
+
+  public async getChecksum() {
     if (this._hashCreater == null) {
       // TODO settings file for algorithm to use
       this._hashCreater = new HashCreater('sha256', this.filePath)
     }
 
-    return this._hashCreater.getChecksum();
+    return await this._hashCreater.getChecksum();
+  }
+
+  get fileName() {
+    return this.filePath.replace(/^.*[\\\/]/, '')
+  }
+
+  public getTotalFileBytes(): number {
+    if (this._fileSize == null) {
+      const fs = require("fs"); //Load the filesystem module
+      const stats = fs.statSync(this.filePath)
+      this._fileSize = stats.size
+    }
+
+    return this._fileSize
   }
 
   public async getFileMimeType() {
     if (this._fileMimeType == null)
       this._fileMimeType = (await FileType.fromFile(this.filePath)).mime
 
-      // console.log('hello3: ' + JSON.stringify(await FileType.fromFile(this.filePath)));
+    // console.log('hello3: ' + JSON.stringify(await FileType.fromFile(this.filePath)));
 
     return this._fileMimeType
   }
