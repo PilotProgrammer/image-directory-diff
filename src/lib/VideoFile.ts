6fae0c5743ee4d722f75997a8510f48cb8a74f6e
PilotProgrammer <17863859+PilotProgrammer@users.noreply.github.com>
2020-12-29 14:43:44 -0500

    WIP

--- src/lib/VideoFile.ts
+++ src/lib/VideoFile.ts
@@ -3,16 +3,13 @@ import { MediaFile } from "./MediaFile";
 const exec = require('child_process').exec;
 
 export class VideoFile extends MediaFile<VideoFile> {
-  private _stats: any
-
-  // private getGenericMediaType(): <T extends MediaFile> {
-  //   return VideoFile
-  // }
+  private _streams: Array<any>
+  private _videoDuration: number
 
   public async equals(otherFile: MediaFile<VideoFile>) {
     if (!(await this.sameVideoDurationInSeconds(otherFile))) return false
     if (!super.equals(otherFile)) return false
-    
+
     return true
   }
 
@@ -20,7 +17,7 @@ export class VideoFile extends MediaFile<VideoFile> {
     const myDuration = await this.getVideoDurationInSeconds()
     const yourDuration = await (<VideoFile>otherFile).getVideoDurationInSeconds()
 
-    if (myDuration.fileName == yourDuration.fileName) {
+    if (myDuration == yourDuration) {
       return true
     } else {
       return false
@@ -29,16 +26,31 @@ export class VideoFile extends MediaFile<VideoFile> {
 
   // https://github.com/Brandon9598/nodejs-video-duration/blob/master/index.js
   public async getVideoDurationInSeconds() {
-    if (this._stats == null) {
+    if (this._streams == null || this._videoDuration == null) {
       const ffprobe = require('ffprobe')
       const ffprobeStatic = require('ffprobe-static');
-      this._stats = await ffprobe(this.filePath, { path: ffprobeStatic.path })
+      this._streams = (await ffprobe(this.filePath, { path: ffprobeStatic.path }) || {}).streams
+      
+      // codec_type = 'video' gives you the video stream!
+      // https://video.stackexchange.com/questions/27546/difference-between-duration-ts-and-duration-in-ffprobe-output
+      const videoStreams = this._streams.filter((value: any, index: number, array: any[]) => {
+        if (value.codec_type == 'video') {
+          return true
+        } else {
+          return false
+        }
+      })
+
+      if (videoStreams.length > 1) {
+        throw new Error(`Too many video steams exist for file: ${this.filePath}`)
+      } else if (videoStreams.length == 0) {
+        throw new Error(`No video streas detected for file: ${this.filePath}`)
+      }
+
+      this._videoDuration = videoStreams[0].duration
     }
 
-    // codec_type = 'video' gives you the video stream!
-
-    // https://video.stackexchange.com/questions/27546/difference-between-duration-ts-and-duration-in-ffprobe-output
-    return this._stats.duration
+    return this._videoDuration
   }
 }
 
