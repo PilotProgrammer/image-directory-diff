caf3f09b17621c79bfa12b2b85b1d3ee80d01d1f
PilotProgrammer <17863859+PilotProgrammer@users.noreply.github.com>
2020-12-31 12:35:38 -0500

    WIP cache and time stats

--- src/lib/MediaFileFactory.ts
+++ src/lib/MediaFileFactory.ts
@@ -7,25 +7,25 @@ const FileType = require('file-type');
 
 export class MediaFileFactory<T extends MediaFile<any>> {
   private _mediaFileCache = new Map<string, MediaFile<T>>()
-  private _useCache = false
+  private _useCache = true
 
   public async createMediaFile(filePath: string) {
     if (this._useCache) {
       if (this._mediaFileCache.get(filePath) == null) {
         console.log(`cache miss ${filePath}`)
-  
+
         const fileMimeType: string = (await FileType.fromFile(filePath)).mime
-  
+
         if ((<string[]>Object.values(VideoFileTypes)).includes(fileMimeType)) {
           this._mediaFileCache.set(filePath, new VideoFile(filePath))
         } else if ((<string[]>Object.values(ImageFileTypes)).includes(fileMimeType)) {
           this._mediaFileCache.set(filePath, new ImageFile(filePath))
-        }  
+        }
       } else {
         console.log(`cache HIT ${filePath}`)
       }
-  
-      return this._mediaFileCache.get(filePath)  
+
+      return this._mediaFileCache.get(filePath)
     } else {
       const fileMimeType: string = (await FileType.fromFile(filePath)).mime
 
@@ -33,7 +33,7 @@ export class MediaFileFactory<T extends MediaFile<any>> {
         return new VideoFile(filePath)
       } else if ((<string[]>Object.values(ImageFileTypes)).includes(fileMimeType)) {
         return new ImageFile(filePath)
-      }  
+      }
       return null
     }
   }
